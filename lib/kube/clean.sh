c13cd5eae41b08fbbcc52b82dff51b742755d121
Ankit Bhawnani <ankit@jalantechnologies.com>
2023-08-12 21:04:17 +0530

    (bug) doppler kube secret is not updated when token is changed (#34)
    * fix: fixed issue with doppler secret not refreshing, added logs for debugging
    
    * tmp: turning off deploy_labels
    
    * reverted back change to deploy_labels

--- lib/kube/clean.sh
+++ lib/kube/clean.sh
@@ -2,28 +2,43 @@
 
 # requires - KUBE_ROOT, KUBE_NS, KUBE_APP, KUBE_ENV
 
+echo "clean :: starting clean up procedure"
+echo "clean :: kube root - $KUBE_ROOT"
+echo "clean :: kube namespace - $KUBE_NS"
+echo "clean :: kube app - $KUBE_APP"
+echo "clean :: kube env - $KUBE_ENV"
+
 kube_pre_clean_script="$KUBE_ROOT/scripts/pre-clean.sh"
 kube_post_clean_script="$KUBE_ROOT/scripts/post-clean.sh"
 
+# deployment pre clean hook
 if [ -f "$kube_pre_clean_script" ]; then
+    echo "clean :: running pre clean up hook - $kube_pre_clean_script"
     source "$kube_pre_clean_script"
 fi
 
+# kubernetes config (shared / env)
 kube_shared_dir="$KUBE_ROOT/shared"
 kube_env_dir="$KUBE_ROOT/$KUBE_ENV"
 
 if [ -d "$kube_shared_dir" ]; then
     for file in "$kube_shared_dir"/*; do
+        echo "clean :: cleaning from shared config - $kube_shared_dir/$file"
         envsubst <"$file" | kubectl delete --ignore-not-found=true -f -
     done
 fi
 
 if [ -d "$kube_env_dir" ]; then
     for file in "$kube_env_dir"/*; do
+        echo "clean :: cleaning from env config - $kube_env_dir/$file"
         envsubst <"$file" | kubectl delete --ignore-not-found=true -f -
     done
 fi
 
+# deployment post clean hook
 if [ -f "$kube_post_clean_script" ]; then
+    echo "clean :: running post clean up hook - $kube_post_clean_script"
     source "$kube_post_clean_script"
 fi
+
+echo "clean :: clean up procedure finished"
--- lib/kube/deploy.sh
+++ lib/kube/deploy.sh
@@ -5,49 +5,55 @@
 # requires - DOCKER_REGISTRY, DOCKER_USERNAME, DOCKER_PASSWORD
 # optional - DOPPLER_TOKEN, DOPPLER_TOKEN_SECRET_NAME, DOPPLER_MANAGED_SECRET_NAME, KUBE_LABELS
 
-echo "deploying to k8"
-echo "kube namespace - $KUBE_NS"
-echo "kube app - $KUBE_APP"
-echo "kube env - $KUBE_ENV"
-echo "kube deployment image - $KUBE_DEPLOYMENT_IMAGE"
-echo "kube ingress hostname - $KUBE_INGRESS_HOSTNAME"
+echo "deploy :: starting deployment procedure"
+echo "deploy :: kube root - $KUBE_ROOT"
+echo "deploy :: kube namespace - $KUBE_NS"
+echo "deploy :: kube app - $KUBE_APP"
+echo "deploy :: kube env - $KUBE_ENV"
+echo "deploy :: kube deployment image - $KUBE_DEPLOYMENT_IMAGE"
+echo "deploy :: kube ingress hostname - $KUBE_INGRESS_HOSTNAME"
 
 kube_pre_deploy_script="$KUBE_ROOT/scripts/pre-deploy.sh"
 kube_post_deploy_script="$KUBE_ROOT/scripts/post-deploy.sh"
 
+# deployment pre deploy hook
 if [ -f "$kube_pre_deploy_script" ]; then
+    echo "deploy :: running pre deploy hook - $kube_pre_deploy_script"
     source "$kube_pre_deploy_script"
 fi
 
-# setup kube namespace for the app
+# kubernetes namespace
+# only created once
 kubectl get namespace "$KUBE_NS" || kubectl create namespace "$KUBE_NS"
 
+# doppler secret - allowing resources to access secrets on doppler
+# see - https://docs.doppler.com/docs/kubernetes-operator
+# created if does not exists, updates in place if updated
 if [[ -n "$DOPPLER_TOKEN" ]]; then
-    # create secrets which will be consumed by the deployment using environment variables
-    # see - https://docs.doppler.com/docs/kubernetes-operator
-
-    kubectl create secret generic "$DOPPLER_TOKEN_SECRET_NAME" \
-        --namespace doppler-operator-system \
-        --from-literal=serviceToken="$DOPPLER_TOKEN" || true
+    kubectl create secret generic "$DOPPLER_TOKEN_SECRET_NAME" --namespace doppler-operator-system --from-literal=serviceToken="$DOPPLER_TOKEN" \
+        --save-config \
+        --dry-run=client \
+        -o yaml | \
+    kubectl apply -f -
 fi
 
-# create secret for accessing docker images from configured docker registry
-# the secret name is 'regcred' and deployments can be configured to use the following secret to pull docker images
-# important - secret might already exist and following will throw an error in that case, handle accordingly
+# docker registry secret - allowing resources to access private registries
+# see - https://stackoverflow.com/questions/45879498/how-can-i-update-a-secret-on-kubernetes-when-it-is-generated-from-a-file
+# created if does not exists, updates in place if updated
 kubectl create secret docker-registry regcred --docker-server="$DOCKER_REGISTRY" --docker-username="$DOCKER_USERNAME" --docker-password="$DOCKER_PASSWORD" -n "$KUBE_NS" \
     --save-config \
     --dry-run=client \
     -o yaml | \
 kubectl apply -f -
 
-# apply kube config (core / shared / env)
-
+# kubernetes config (core / shared / env)
 kube_core_dir="$KUBE_ROOT/core"
 kube_shared_dir="$KUBE_ROOT/shared"
 kube_env_dir="$KUBE_ROOT/$KUBE_ENV"
 
 if [ -d "$kube_core_dir" ]; then
     for file in "$kube_core_dir"/*; do
+        echo "deploy :: deploying from core config - $kube_core_dir/$file"
         envsubst <"$file" | kubectl apply -f -
 
         if [ -n "$KUBE_LABELS" ]; then
@@ -58,6 +64,7 @@ fi
 
 if [ -d "$kube_shared_dir" ]; then
     for file in "$kube_shared_dir"/*; do
+        echo "deploy :: deploying from shared config - $kube_shared_dir/$file"
         envsubst <"$file" | kubectl apply -f -
 
         if [ -n "$KUBE_LABELS" ]; then
@@ -68,6 +75,7 @@ fi
 
 if [ -d "$kube_env_dir" ]; then
     for file in "$kube_env_dir"/*; do
+        echo "deploy :: deploying from env config - $kube_env_dir/$file"
         envsubst <"$file" | kubectl apply -f -
 
         if [ -n "$KUBE_LABELS" ]; then
@@ -76,8 +84,10 @@ if [ -d "$kube_env_dir" ]; then
     done
 fi
 
+# deployment post deploy hook
 if [ -f "$kube_post_deploy_script" ]; then
+    echo "deploy :: running post deploy hook - $kube_post_deploy_script"
     source "$kube_post_deploy_script"
 fi
 
-echo "deployed to - https://$KUBE_INGRESS_HOSTNAME"
+echo "deploy :: deployment procedure finished"
